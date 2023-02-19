c1c80d157c9cc971c784a3ca56187f2f43ffb230
Holzelh <spenser.vale.com@gmail.com>
2023-02-19 15:01:20 -0600

    class still needs to be finished

--- PORTFOLIOS/portfolio3/app/src/main/java/z1959955/cs/niu/edu/portfolio3/Burger.java
+++ PORTFOLIOS/portfolio3/app/src/main/java/z1959955/cs/niu/edu/portfolio3/Burger.java
@@ -1,4 +1,15 @@
 package z1959955.cs.niu.edu.portfolio3;
 
 public class Burger {
+    private int burgerPattyCalories;
+    private int cheeseCalories;
+    private int baconCalories;
+    private int specialSauceCalories;
+    private final int BEEF_PATTY_CALORIES = 247;
+    private final int TURKEY_PATTY_CALORIES = 193;
+    private final int VEGGIE_PATTY_CALORIES = 177;
+    private final int BACON_CALORIES = 541;
+    private final int CHEDDAR_CALORIES = 402;
+    private final int MOZZARELLA_CALORIES = 280;
+
 }
