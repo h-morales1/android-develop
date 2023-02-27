cbbc1b5e32115072004bff23cbc4c17b9f2a2ca5
Holzelh <spenser.vale.com@gmail.com>
2023-02-27 04:21:34 -0600

    modifications to portfolio3 Burger class

--- PORTFOLIOS/portfolio3/app/src/main/java/z1959955/cs/niu/edu/portfolio3/Burger.java
+++ PORTFOLIOS/portfolio3/app/src/main/java/z1959955/cs/niu/edu/portfolio3/Burger.java
@@ -1,6 +1,7 @@
 package z1959955.cs.niu.edu.portfolio3;
 
 public class Burger {
+    private int totalCalories;
     private int burgerPattyCalories;
     private int cheeseCalories;
     private int baconCalories;
@@ -11,5 +12,49 @@ public class Burger {
     private final int BACON_CALORIES = 541;
     private final int CHEDDAR_CALORIES = 402;
     private final int MOZZARELLA_CALORIES = 280;
+
+    public Burger() {
+        setBurgerPattyCalories(0);
+        setCheeseCalories(0);
+        setBaconCalories(0);
+        setSpecialSauceCalories(0);
+        calculateTotalCalories();
+    }
+
+    private void setBurgerPattyCalories(int pattyCal) {
+        burgerPattyCalories = pattyCal;
+    }
+
+    public void setBPattyBeef() {
+        setBurgerPattyCalories(BEEF_PATTY_CALORIES);
+    }
+
+    public void setBPattyVeggie() {
+        setBurgerPattyCalories(VEGGIE_PATTY_CALORIES);
+    }
+
+    public void setBPattyTurkey() {
+        setBurgerPattyCalories(TURKEY_PATTY_CALORIES);
+    }
+
+    public void setCheeseCalories(int cheeseCal) {
+        cheeseCalories = cheeseCal;
+    }
+
+    public void setBaconCalories(int baconCal) {
+        baconCalories = baconCal;
+    }
+
+    public void setSpecialSauceCalories(int sauceCal) {
+        specialSauceCalories = sauceCal;
+    }
+
+    public void calculateTotalCalories() {
+        totalCalories = burgerPattyCalories + cheeseCalories + baconCalories + specialSauceCalories;
+    }
+
+    public int getTotalCalories() {
+        return totalCalories;
+    }
 
 }
--- PORTFOLIOS/portfolio3/app/src/main/java/z1959955/cs/niu/edu/portfolio3/MainActivity.java
+++ PORTFOLIOS/portfolio3/app/src/main/java/z1959955/cs/niu/edu/portfolio3/MainActivity.java
@@ -3,12 +3,57 @@ package z1959955.cs.niu.edu.portfolio3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    RadioButton beefPattyRB, turkeyRB, veggieRB;
+    CheckBox baconCB;
+    RadioButton noCheeseRB, cheddarRB, mozzarellaRB;
+    SeekBar specialSauceSB;
+    Burger userBurger;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userBurger = new Burger();
+    }
+
+    public void onClickPattyRB(View view) { // on click of first radiobutton group, which patty to use
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // check which radio button from the group was checked
+        switch(view.getId()) {
+            case R.id.beefPattyRB:
+                if(checked) {
+                    // apply beef patty calories
+                    userBurger.setBPattyBeef();
+                }
+                break;
+            case R.id.turkeyPattyRB:
+                if(checked) {
+                    // apply turkey patty calories
+                    userBurger.setBPattyTurkey();
+                }
+                break;
+            case R.id.veggiePattyRB:
+                if(checked) {
+                    // apply veggie patty calories
+                    userBurger.setBPattyVeggie();
+                }
+                break;
+        }
+    }
+
+    public void onBaconCbx(View view) {
+        boolean checked = ((CheckBox) view).isChecked();
+        if(checked) {
+            //
+        }
     }
 }
