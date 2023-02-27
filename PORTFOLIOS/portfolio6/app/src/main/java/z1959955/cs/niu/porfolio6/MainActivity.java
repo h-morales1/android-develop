aede5210e66107bef5d320af91d4fe3f0420d886
Holzelh <spenser.vale.com@gmail.com>
2023-02-27 04:20:26 -0600

    added portfolio6

---
+++ PORTFOLIOS/portfolio6/app/src/main/java/z1959955/cs/niu/porfolio6/MainActivity.java
@@ -0,0 +1,14 @@
+package z1959955.cs.niu.porfolio6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
